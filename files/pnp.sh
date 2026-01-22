25bd0a62169ba70d24343250ee36c157343b02b7
Moojid Hamid <moojid@gmail.com>
2026-01-22 13:11:17 -0700

    removed files and variables not needed for noortv project

--- files/pnp.sh
+++
@@ -1,3 +0,0 @@
-IP=`ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
-pnp=$(upnpc  -a $IP 22 2222 TCP 3595)
-curl -X POST -F data="$pnp" -F store="$CPID" -F ip="$IP" $PING_URL| json_pp
--- files/tm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-      while [ "$(hostname -I)" = "" ]; do
-        sleep 1
-      done
-      tmux new-session -d  'pianobar'
-      tmux split-window -v 'cd ~/Patiobar && node index.js'
-      tmux split-window -h  "bluetoothctl connect $SPK_MAC"
